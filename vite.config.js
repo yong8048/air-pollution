f0522716669972dc3df9ca8aa7a590f318c36e6e
yong8048 <water8048@naver.com>
2023-09-19 15:11:16 +0900

    feat: 에러수정

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: [{ find: '@', replacement: '/src' }],
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "https://apis.data.go.kr",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
+        ws: true,
+      },
     },
-    server: {
-        port: 3000,
-        proxy: {
-            '/api': {
-                target: 'https://apis.data.go.kr',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
-            }
-        }
-    },
-})
+  },
+});
